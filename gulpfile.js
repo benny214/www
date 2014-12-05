5bd1be7fea967c0ac2dd4374050c7bf6ce3c4ce0
Viktor Tumanov <viktor_tumanov@etb-lab.com>
2014-12-05 15:53:48 +0300

    gulpAdd

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+    concatCss = require('gulp-concat-css');
+    // uncss = require('gulp-uncss');
+    minifyCSS = require('gulp-minify-css');
+    rename = require("gulp-rename");
+    notify = require("gulp-notify");
+
+gulp.task('default', function() {
+  gulp.src('css/*.css')
+    .pipe(concatCss("bundle.css"))
+    // .pipe(uncss({
+    //         html: ['index.html']}))
+    .pipe(minifyCSS())
+    .pipe(rename("bundle.min.css"))
+    .pipe(gulp.dest('out/'))
+    .pipe(notify("Done!"));
+});
+
+gulp.task('watch', function(){
+    gulp.watch('css/*css', ['default']);
+});
