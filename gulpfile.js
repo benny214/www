afad88596977e3b59434221688f31ba9f4eb4415
Viktor Tumanov <viktor_tumanov@etb-lab.com>
2014-12-08 12:29:55 +0300

    hotfix

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,15 @@ var gulp = require('gulp');
     minifyCSS = require('gulp-minify-css');
     rename = require("gulp-rename");
     notify = require("gulp-notify");
+    autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function() {
   gulp.src('css/*.css')
     .pipe(concatCss("bundle.css"))
+    .pipe(autoprefixer({
+            browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1'],
+            cascade: false
+        }))
     // .pipe(uncss({
     //         html: ['index.html']}))
     .pipe(minifyCSS())
